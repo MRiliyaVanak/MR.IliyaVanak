25f82752ca3a26cf5059dc0907332218dbd55cd7
MR.iliyavanal <iv.sn82211>
2023-08-04 08:45:01 +0330

    salam

---
+++ main.py
@@ -0,0 +1,90 @@
+# a = int(input())
+# b = 0
+# while a != 0 :
+#     d = a%10
+#     a = a//10
+#     b = b*10+d
+
+# a =b 
+# b = 0
+# while a != 0 :
+#     d = a%10
+#     a = a//10
+#     #b = b*10+d
+#     print(f"{d}:",end="")
+#     i = 0
+#     while i < d :
+#         print(d , end="")
+#         i+=1 
+#     print()
+
+# number = int(input("please enter your number ..."))
+# i = 2
+# p = True
+# while i<number :
+#     if number%i == 0 :
+#         p = False
+#     i = i +1
+# print(p)
+
+# n = int(input())
+# i = 2
+# p =True
+# while i<n :
+#     if n%i == 0 :
+#         p=False
+#     i+=1
+# print(p)
+
+# a = int(input())
+# b = int(input())
+# if a>b :
+#     a,b=b,a
+# n=a
+# while n<=b:
+#     i = 2
+#     p =True
+#     while i<n**5.0+1:
+#         if n%i==0:
+#             p = False
+#         i+=1 
+#     if p and n!=1:
+#         print(n)
+#     n+=1
+# def my_name(NAME):
+#     print("HELLO MY NAME IS "+ NAME)
+
+# my_name(input("INTER YOUR NAME "))
+# def nigga(a,b):
+#     return a**b
+# z = nigga(2,3)
+# print(z)
+# def nigger(n):
+#     if n==1:
+#         return False
+#     i = 2
+#     p =True
+#     while i<=n**0.5+1:
+#         if n%i==0:
+#             return False
+#         i+=1 
+#         if p :
+#             print(p)
+#         return True
+    
+# nigger(12)
+# n = int(input())
+# i=0
+# while i<n:
+#     j=0
+#     while j<n:
+#         if i==0 or i ==n-1:
+#             print("*",end="")
+#         else:
+#             if j==0 or j==n-1:
+#                 print("*",end="")
+#             else:
+#                 print(" ",end="")
+#         j+=1
+#     print()
+#     i+=1\
